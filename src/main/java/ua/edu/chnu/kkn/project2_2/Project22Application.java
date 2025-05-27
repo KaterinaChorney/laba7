31cd836527ca034c4eb229f2464f6c6a9045930d
Katerina Chorney <katyaciti7@gmail.com>
2025-05-27 13:03:26 +0300

    Inital commit.

---
+++ src/main/java/ua/edu/chnu/kkn/project2_2/Project22Application.java
@@ -0,0 +1,13 @@
+package ua.edu.chnu.kkn.project2_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project22Application.class, args);
+    }
+
+}
---
+++ src/test/java/ua/edu/chnu/kkn/project2_2/Project22ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.chnu.kkn.project2_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
